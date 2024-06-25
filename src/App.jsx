12f48d01321de5063435c852c31476ea75901c58
Ivan Konderev <konderev@aheadworks.com>
2024-06-25 12:08:19 +0300

    Add FlowBite and FlowBite React lib over tailwind and react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import Header from "./components/Header"
+import { Navbar, Footer } from "flowbite-react";
+import {BsGithub} from "react-icons/bs"
+import Main from "./components/Main";
 
+const currentYear = new Date().getFullYear();
 function App() {
-  
   return (
-  <>
-    <Header />
-  </>  
-  )
+    <>
+      <Navbar className="max-w-screen-xl mx-auto p-4">
+        <Navbar.Brand href="https://flowbite-react.com">
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            React Meal Shop
+          </span>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Navbar.Link href="#" active>
+            Home
+          </Navbar.Link>
+          <Navbar.Link href="#">About</Navbar.Link>
+          <Navbar.Link href="#">Services</Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Contact</Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+      <Main />
+      <Footer className="max-w-screen-xl mx-auto p-4 fixed bottom-0 left-1/2 transform -translate-x-1/2">
+        <Footer.Copyright href="https://github.com/kondzerau-ivan" by=" Ivan Kondzerau" year={currentYear} />
+        <Footer.Icon href="https://github.com/kondzerau-ivan/react-meal-shop" icon={BsGithub} target="_blank" />
+      </Footer>
+    </>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 export default {
   content: [
     "./index.html",
@@ -6,7 +7,31 @@ export default {
     'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [
     require('flowbite/plugin')
